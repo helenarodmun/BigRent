cfcaff3b140c4c00970a24e72a1a3c592fc99593
helenarodmun <helenarodmun@gmail.com>
2023-04-20 11:15:50 +0100

    Modificación tabla contrato, cálculo semanas, dias import contrato

--- resources/js/components/contratos/VerContrato.jsx
+++ resources/js/components/contratos/VerContrato.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import {Container} from 'react-boostrap'
 export default function VerContrato() {
     const { contrato, semanas, días, importe} = usePage();
     return(
-       <Container></Container>
+       <div></div>
     )
 }
